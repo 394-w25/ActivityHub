791661acdc4077ff237b643a1da72a00bf913a24
dweber2022 <102439573+dweber2022@users.noreply.github.com>
2025-02-19 11:55:45 -0600

    Add activity DB utils (#6)
    * add and init firebase project
    
    * move files to proper directories
    
    * actually move files to their proper directories
    
    * add path aliases for src, components, hooks, and utils
    
    * move hidden files to proper directories
    
    * remove moved files
    
    * add activity db utils
    
    * Remove placeholder tests
    
    * Add welcome page (#3)
    
    * Welcoming page and onboarding
    
    * firebase set up
    
    * WelcomePage
    
    * OnboardingPage
    
    * unique picture for all activties
    
    * Remove placeholder tests
    
    * fix placeholder test suite
    
    * fix formatting
    
    ---------
    
    Co-authored-by: dweber2022 <102439573+dweber2022@users.noreply.github.com>
    
    * Add Tailwind CLI (#5)
    
    * add and init firebase project
    
    * actually move files to their proper directories
    
    * move OnboardingFlow into components dir; fix components alias in Vite config; remove unused asset
    
    * add path alias for assets; fix asset import paths in OnboardingFlow
    
    ---------
    
    Co-authored-by: dweber <102439573+dcheng2022@users.noreply.github.com>
    Co-authored-by: Zexuan Zhou <94760515+HZZX0318@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,14 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@assets": path.resolve(__dirname, "./assets"),
+    },
   },
 });
