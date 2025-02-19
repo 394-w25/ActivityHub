bc13cd35268600a953459f3ec69abbbe1c13182c
Zexuan Zhou <94760515+HZZX0318@users.noreply.github.com>
2025-02-19 10:20:44 -0600

    Add welcome page (#3)
    * Welcoming page and onboarding
    
    * firebase set up
    
    * WelcomePage
    
    * OnboardingPage
    
    * unique picture for all activties
    
    * Remove placeholder tests
    
    * fix placeholder test suite
    
    * fix formatting
    
    ---------
    
    Co-authored-by: dweber2022 <102439573+dweber2022@users.noreply.github.com>

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,3 +1,9 @@
 import { describe, expect, test } from "vitest";
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
+
+describe("placeholder suite", () => {
+  test("placeholder test", () => {
+    expect(true);
+  });
+});
