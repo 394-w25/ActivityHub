3f7c4b00cd02347c73d3c34525d6b6a8966ee499
dweber <102439573+dcheng2022@users.noreply.github.com>
2025-02-25 13:39:46 -0600

    add Leaflet GeoSearch; use geosearch in activity form

--- src/components/ActivityForm.jsx
+++ src/components/ActivityForm.jsx
@@ -9,6 +9,8 @@ import {
   TooltipContent,
   TooltipProvider,
 } from "@/components/ui/tooltip";
+import { MapContainer, TileLayer } from "react-leaflet";
+import MapSearchField from "@components/MapSearchField";
 import { useAuthState, useDbUpdate } from "../hooks/firebase";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
@@ -17,9 +19,9 @@ const ActivityForm = ({ onSuccess }) => {
   const [updateData] = useDbUpdate(
     user ? `users/${user.uid}/activities` : null,
   );
-
   const [title, setTitle] = useState("");
   const [image, setImage] = useState(null);
+  const [activityLocation, setActivityLocation] = useState({});
   const [imagePreview, setImagePreview] = useState(null);
   const [uploading, setUploading] = useState(false);
   const [errors, setErrors] = useState({});
@@ -43,7 +45,8 @@ const ActivityForm = ({ onSuccess }) => {
     const formData = new FormData(e.target);
     const title = formData.get("title");
     const description = formData.get("description");
-    const location = formData.get("location");
+    const location = activityLocation.label;
+    const coords = [activityLocation.x, activityLocation.y];
     const groupSize = formData.get("groupSize");
     const eventTimestamp = formData.get("eventTimestamp");
 
@@ -53,6 +56,10 @@ const ActivityForm = ({ onSuccess }) => {
       validationErrors.title = "Title is required.";
     } else if (title.length > 50) {
       validationErrors.title = "Title must be less than 50 characters.";
+    }
+
+    if (Object.keys(activityLocation).length === 0) {
+      validationErrors.location = "Location is required.";
     }
 
     const parsedGroupSize = parseInt(groupSize, 10);
@@ -98,6 +105,7 @@ const ActivityForm = ({ onSuccess }) => {
         title,
         description,
         location,
+        coords,
         groupSize: parseInt(groupSize, 10),
         eventTimestamp,
         creationTimestamp: Date.now(),
@@ -163,13 +171,20 @@ const ActivityForm = ({ onSuccess }) => {
 
       {/* Location */}
       <div>
-        <Input
-          id="location"
-          name="location"
-          type="text"
-          placeholder="Location"
-          required
-        />
+        <MapContainer
+          center={location?.coords || [42.056, -87.6755]}
+          zoom={17}
+          className="h-128"
+        >
+          <MapSearchField setActivityLocation={setActivityLocation} />
+          <TileLayer
+            attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
+            url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+        {errors.location && (
+          <p className="text-red-500 text-sm">{errors.location}</p>
+        )}
       </div>
 
       {/* Group Size + Tooltip */}
---
+++ src/components/MapSearchField.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
+import { useMap } from "react-leaflet";
+import "leaflet-geosearch/dist/geosearch.css";
+
+const MapSearchField = ({ setActivityLocation }) => {
+  const provider = new OpenStreetMapProvider();
+  const searchControl = new GeoSearchControl({
+    provider: provider,
+    style: "bar",
+  });
+  const map = useMap();
+  map.on("geosearch/showlocation", (e) => {
+    setActivityLocation({
+      label: e.location.label,
+      coords: [e.location.x, e.location.y],
+    });
+  });
+  useEffect(() => {
+    map.addControl(searchControl);
+    return () => map.removeControl(searchControl);
+  }, []);
+  return null;
+};
+
+export default MapSearchField;
