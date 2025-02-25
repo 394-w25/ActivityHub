147f1e739af12df2c390a2c3b8528f05bc27c124
dweber <102439573+dcheng2022@users.noreply.github.com>
2025-02-25 11:28:51 -0600

    change tile server

--- src/components/ActivitiesMap.jsx
+++ src/components/ActivitiesMap.jsx
@@ -8,10 +8,10 @@ const ActivitiesMap = () => {
   });
   return (
     <div>
-      <MapContainer center={userCoords} zoom={16} className="h-128">
+      <MapContainer center={userCoords} zoom={17} className="h-128">
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
+          url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}.png"
         />
         <Marker position={userCoords}>
           <Popup>Bouldering 3-5pm at Movement Lincoln Park!</Popup>
