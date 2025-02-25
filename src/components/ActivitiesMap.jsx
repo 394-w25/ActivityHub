461f395c15f8dbf4bde5cdf2eb87975585a0cd01
dweber <102439573+dcheng2022@users.noreply.github.com>
2025-02-24 18:18:00 -0600

    add placeholder map; add request for device location

--- src/components/ActivitiesMap.jsx
+++ src/components/ActivitiesMap.jsx
@@ -1,22 +1,20 @@
+import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const ActivitiesMap = () => {
+  const [userCoords, setUserCoords] = useState([41.908, -87.649]);
+  navigator.geolocation.getCurrentPosition((position) => {
+    setUserCoords([position.coords.longitude, position.coords.latitude]);
+  });
   return (
     <div>
-      <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
-        className="h-48"
-      >
+      <MapContainer center={userCoords} zoom={16} className="h-128">
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
+        <Marker position={userCoords}>
+          <Popup>Bouldering 3-5pm at Movement Lincoln Park!</Popup>
         </Marker>
       </MapContainer>
     </div>
