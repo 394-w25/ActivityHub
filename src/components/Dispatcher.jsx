b61c4a414343f66770a899f73500903010721302
Ellie Li <ellieli@Ellies-MacBook-Pro-182.local>
2025-03-05 22:54:04 -0600

    sign in and sign up changes

--- src/components/Dispatcher.jsx
+++ src/components/Dispatcher.jsx
@@ -9,7 +9,7 @@ import ChatsListPage from "@/pages/ChatsListPage";
 import SignInPage from "@/pages/SignInPage";
 import SignUpPage from "@/pages/SignUpPage";
 import PhoneSignUpPage from "@/pages/PhoneSignUpPage";
-import OnboardingPage from "@/pages/OnboardingPage";
+// import OnboardingPage from "@/pages/OnboardingPage";
 import LandingPage from "@/pages/LandingPage";
 
 const Dispatcher = () => {
@@ -22,7 +22,7 @@ const Dispatcher = () => {
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/phone-signup" element={<PhoneSignUpPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/onboarding" element={<OnboardingFlow />} />
         <Route path="/user_profile/:id" element={<UserProfile />} />
         <Route path="/notification" element={<NotificationPage />} />
         <Route path="/chats" element={<ChatsListPage />} />
